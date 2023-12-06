30639801f6bccf940f12ed628b56d5830a721461
farrukh.khan <farrukh.khan@sybrid.com>
2023-12-06 13:56:49 +0500

    Error Handling added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,22 @@
 import User from '../models/User.js'
 import {StatusCodes} from 'http-status-codes'
+import {BadRequestError} from '../errors/index.js'
+
+
 
 const register = async (req, res) => {
     // res.send('register user')
-    
-        const user = await User.create(req.body)
+        const {name, email, password} = req.body
+        if(!name || !email || !password) {
+            throw new BadRequestError('Please Provide all values')
+        }
+
+        // const user = await User.create(req.body)
+        const userAlreadyExists = await User.findOne({email});
+        if (userAlreadyExists) {
+            throw new BadRequestError('Email already in use')
+        }
+        const user = await User.create({name, email, password})
         res.status(StatusCodes.CREATED).json({ user })
 }
 
